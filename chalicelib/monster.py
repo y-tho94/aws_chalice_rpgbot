218bbf442d1f1d4e01de8072bb0b8823884ea404
ytho94 <jakefreydo@gmail.com>
2021-02-26 12:58:43 +0000

    added airtable crud

--- chalicelib/monster.py
+++ chalicelib/monster.py
@@ -1,64 +1,124 @@
 from chalice import Blueprint
 import json
+from airtable import Airtable
 import os
 
 monsterRoutes = Blueprint(__name__)
 
-#instantiate Fauna DB
-client = FaunaClient(secret= os.environ.get('faunaMonster'))
+#instantiate airtable (info from airtable.com/api)
+baseKey = os.environ['MONSTERS_BASE_KEY']
+secret = os.environ['AIRTABLE_API_KEY']
+tbl = Airtable(baseKey, "Monsters", secret)
 
 #Create Monster
 @monsterRoutes.route('/', methods=['POST'])
 def create():
-  req = json.loads(monsterRoutes.current_request.raw_body)
+  try:
+    req = json.loads(monsterRoutes.current_request.raw_body)
 
-  rowCount = client.query(
-    q.count(q.documents(q.collection("Monsters")))
-  )
-  # newIndex = rowCount + 1
+    newMonster = {
+      "Name"          :req['name'],
+      "Image"         :req['img'],
+      "Type"          :req['type'],
+      "BaseHP"        :req['HP'],
+      "BaseATK"       :req['ATK'],
+      "BaseDEF"       :req['DEF'],
+      "BaseSPD"       :req['SPD'],
+      "BaseSPEC"      :req['SPEC'],
+      "BaseWealth"    :req['Wealth'],
+      "BaseXP"        :req['XP'],
+      "lvlMultiplier" :req['lvlMult']
+    }
 
-  # newMonster = {
-  #   "id"      :newIndex,
-  #   "name"    :req['name'],
-  #   "type"    :req['type'],
-  #   "HP"      :req['HP'],
-  #   "ATK"     :req['ATK'],
-  #   "DEF"     :req['DEF'],
-  #   "SPD"     :req['SPD'],
-  #   "SPEC"    :req['SPEC'],
-  #   "Wealth"  :req['Wealth'],
-  #   "XP"      :req['XP'],
-  #   "lvlMult" :req['lvlMult']
-  # }
+    response = tbl.insert(newMonster, True) 
 
-  # response = client.query(
-#   q.Create(
-  #     q.collection("Monsters"),
-  #     {"data": newMonster}
-  #   )
-  # )
-
-  return rowCount
+  except Exception as ex:
+    response = {
+      "data":{
+        "status" : 500,
+        "message" : ex
+      }
+    }
+  finally:
+    return response
 
 #Get All
 @monsterRoutes.route('/')
 def getAll():
-  res = 4
-  data = res['Items']
-  return data
+  try:
+    response = tbl.get_all() 
+
+  except Exception as ex:
+    response = {
+      "data":{
+        "status" : 500,
+        "message" : ex
+      }
+    }
+  finally:
+    return response
 
 #Get one
 @monsterRoutes.route('/{id}')
 def getOne(id):
-  res = 4
-  return res['Item']
+  try:
+    response = tbl.get(id) 
+
+  except Exception as ex:
+    response = {
+      "data":{
+        "status" : 500,
+        "message" : ex
+      }
+    }
+  finally:
+    return response
 
 #Update
 @monsterRoutes.route('/{id}', methods=['PUT'])
 def update(id, request):
-  return {'updated':id}
+  try:
+    req = json.loads(monsterRoutes.current_request.raw_body)
+
+    monster = {
+      "Name"          :req['name'],
+      "Image"         :req['img'],
+      "Type"          :req['type'],
+      "BaseHP"        :req['HP'],
+      "BaseATK"       :req['ATK'],
+      "BaseDEF"       :req['DEF'],
+      "BaseSPD"       :req['SPD'],
+      "BaseSPEC"      :req['SPEC'],
+      "BaseWealth"    :req['Wealth'],
+      "BaseXP"        :req['XP'],
+      "lvlMultiplier" :req['lvlMult']
+    }
+
+    response = tbl.update(id, monster, True) 
+
+  except Exception as ex:
+    response = {
+      "data":{
+        "status" : 500,
+        "message" : ex
+      }
+    }
+  finally:
+    return response
+
 
 #Delete
 @monsterRoutes.route('/{id}', methods=['DELETE'])
 def delete(id):
-  return {'deleted': id}
+  try:
+    response = tbl.delete(id) 
+
+  except Exception as ex:
+    response = {
+      "data":{
+        "status" : 500,
+        "message" : ex
+      }
+    }
+  finally:
+    return response
